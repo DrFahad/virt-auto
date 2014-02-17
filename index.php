53b89b49781ed3d2fc61505c5bc914d3d5a35e04
fahad, working on the development server <root@dr-high.com>
2014-02-17 16:10:49 +1000

    add frontpage

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link href="css/main.css" type="text/css" rel="stylesheet">
+
+<title>Img-r-us</title>
+
+</head>
+<body>
+    <div class="left"><h1>Upload Files </h1></div>
+    <div>
+    
+    	
+        <!-- <p>.</p>
+    
+    	<h2 id="gal-one"> <span></span></h2> -->
+        <ul class="gal-1">
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+            <li><img src="images/thumbs/pngimg.png" alt=""></li>
+            
+        </ul>
+    </div>
+</body>
+
+
+<?php
+
+
+?>
---
+++ thumbnail_1.php
@@ -0,0 +1,116 @@
+<html>
+    <body>
+        <?php
+//phpinfo();
+
+        ini_set('max_execution_time', 300);
+
+        $documentRoot = $_SERVER['DOCUMENT_ROOT'];
+//$projectRoot='/week4task1-2';
+        $imageDir = "/images";
+        $htmlImageDir = "images";
+        //$imageName = $_GET['id'];
+        $relativeImgPath = "";
+        $imgExt = "";
+        $exists = False;
+        $thumbsDir = "." . $imageDir . "/thumbs";
+        $quality = 100;
+        $squareSize = 150;
+        $totalImages = 10;
+
+        /*
+        if (empty($imageName) || !isset($imageName)) {
+            echo "\nYou didn't specify an ID";
+            exit;
+        }
+        */
+        $imageSet=glob('.' . $imageDir . '/*.*') ;
+        //$rand_keys = array_rand($imageSet, $totalImages);
+        //echo $imageSet[rand(0, count($imageSet)-1)]."<br/>";
+        shuffle($imageSet);
+        $randomElements = array_slice($imageSet, 0, 10);
+        foreach ($randomElements as $key => $value) {
+            //echo "Key= ".$key."<br/>";
+            echo "Value= ".$value."<br/>";
+        }
+//glob is used to search the image directory for filename
+        foreach ($rand_keys as $relativeImagePath) {
+            echo $relativeImagePath."<br/>";
+            if (strcasecmp(mime_content_type($relativeImagePath), "image/jpeg") == 0 || strcasecmp(mime_content_type($relativeImagePath), "image/jpg") == 0 || strcasecmp(mime_content_type($relativeImagePath), "image/png") == 0 || strcasecmp(mime_content_type($relativeImagePath), "image/gif") == 0) {
+                $exists = True;
+                $relativeImgPath = $relativeImagePath;
+                echo "Filename: " . $relativeImagePath . "<br />";
+                $path_parts = pathinfo($relativeImagePath);
+                $imgExt = '.' . $path_parts['extension'];
+                $htmlPath = $htmlImageDir . "/" . $imageName . $imgExt;
+                if ($exists) {
+                    create_thumbnail($imageName . $imgExt, "." . $imageDir, $thumbsDir, $squareSize, $quality);
+                    if (file_exists($thumbsDir . "/" . $imageName . $imgExt)) {
+                        ?>
+                        <img border=1 src="<?php echo $thumbsDir . "/" . $imageName . $imgExt; ?>"/>
+                        <?php
+                    } else {
+                        echo "\nProblem creating thumbnail";
+                    }
+                } else {
+                    print "\nID does not exist";
+                }
+            }
+        }
+
+//$fqImagePath=$documentRoot.$projectRoot.$imageDir."/".$imageName.$imgExt;
+//echo $htmlPath;
+
+
+        function create_thumbnail($file, $photos_dir, $thumbs_dir, $square_size, $quality) {
+            $pp = pathinfo($file);
+            $imgExt = $pp['extension'];
+            //check if thumb exists
+            if (!file_exists($thumbs_dir . "/" . $file)) {
+                //get image info
+                list($width, $height, $type, $attr) = getimagesize($photos_dir . "/" . $file);
+
+                //set dimensions
+                if ($width > $height) {
+                    $width_t = $square_size;
+                    //respect the ratio
+                    $height_t = round($height / $width * $square_size);
+                    //set the offset
+                    $off_y = ceil(($width_t - $height_t) / 2);
+                    $off_x = 0;
+                } elseif ($height > $width) {
+                    $height_t = $square_size;
+                    $width_t = round($width / $height * $square_size);
+                    $off_x = ceil(($height_t - $width_t) / 2);
+                    $off_y = 0;
+                } else {
+                    $width_t = $height_t = $square_size;
+                    $off_x = $off_y = 0;
+                }
+                if (strcasecmp($imgExt, "jpg") == 0 || strcasecmp($imgExt, "jpeg") == 0)
+                    $thumb = imagecreatefromjpeg($photos_dir . "/" . $file);
+                elseif (strcasecmp($imgExt, "gif") == 0)
+                    $thumb = imagecreatefromgif($photos_dir . "/" . $file);
+                elseif (strcasecmp($imgExt, "png") == 0)
+                    $thumb = imagecreatefrompng($photos_dir . "/" . $file);
+
+                $thumb_p = imagecreatetruecolor($square_size, $square_size);
+                //default background is black
+                $bg = imagecolorallocate($thumb_p, 255, 255, 255);
+                imagefill($thumb_p, 0, 0, $bg);
+                imagecopyresampled($thumb_p, $thumb, $off_x, $off_y, 0, 0, $width_t, $height_t, $width, $height);
+                imagejpeg($thumb_p, $thumbs_dir . "/" . $file, $quality);
+                /*
+                  if(strcasecmp($imgExt,"jpg")==0 || strcasecmp($imgExt,"jpeg")==0)                         imagejpeg($thumb_p,$thumbs_dir."/".$file,$quality);
+                  elseif(strcasecmp($imgExt,"gif")==0 )
+                  imagegif($thumb_p,$thumbs_dir."/".$file,$quality);
+                  elseif(strcasecmp($imgExt,"png")==0 ){
+                  imagealphablending($thumb_p,false);
+                  imagesavealpha($thumb_p,true);
+                  imagepng($thumb_p,$thumbs_dir."/".$file,$quality);
+                  } */
+            }
+        }
+        ?>
+    </body>
+</html>
